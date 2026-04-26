37dd1bf46da5ad5b62f7ea8edf25460015918d23
javiernr02 <javnunrui@alum.us.es>
2026-04-26 16:05:34 +0200

    feat: add recommender logic improvements

--- app/recommender.py
+++ app/recommender.py
@@ -1,9 +1,43 @@
 import pandas as pd
 
 movies = pd.read_csv('data/movies.csv')
+ratings = pd.read_csv('data/ratings.csv')
 
-def movies_by_genre(genre, n):
-    movies['genres_list'] = movies['genres'].str.split('|')
-    movies_filtered = movies[movies['genres_list'].apply(lambda x: genre in x)]
+movies['genres_list'] = movies['genres'].str.split('|')
+
+movies['year'] = movies['title'].str.extract(r'\((\d{4})\)')[0].fillna('0').astype(int)
+
+def movies_by_genre(genre):
+    movies_filtered = movies[movies['genres_list'].apply(lambda x: genre in x if isinstance(x, list) else False)]
     
-    return movies_filtered['title'].head(n).tolist()
+    return movies_filtered['title'].tolist()
+
+def movies_by_year(year):
+    movies_filtered = movies[movies['year'] == int(year)]
+    
+    return movies_filtered['title'].tolist()
+
+def movies_by_genre_and_year(genre, year):
+    filtered = movies[
+        (movies['genres_list'].apply(lambda x: genre in x)) & (movies['year'] == int(year))]
+    
+    return filtered['title'].tolist()
+
+def movie_info_by_title(title):
+    movie =  movies[movies['title'].str.lower().str.contains(title.lower(), na=False)]
+    
+    if movie.empty:
+        return None
+
+    movie = movie.iloc[0]
+    
+    movie_id = movie['movieId']
+    
+    movie_ratings = ratings[ratings['movieId'] == movie_id]
+    
+    if not movie_ratings.empty:
+        avg_rating = movie_ratings['rating'].mean()
+    else:
+        avg_rating = None
+    
+    return {'title': movie['title'], 'genres': movie['genres'], 'year': movie['year'], 'rating': round(avg_rating, 2) if avg_rating else None}
